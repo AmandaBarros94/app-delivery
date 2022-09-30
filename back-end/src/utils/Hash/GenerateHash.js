f7ee243fb4576efc6fabe5d5a6718a9693a3fb5f
Sergio Francisco <spinheiro.lf@gmail.com>
2022-09-30 18:41:28 -0300

    consertando o login

--- back-end/src/utils/Hash/GenerateHash.js
+++ back-end/src/utils/Hash/GenerateHash.js
@@ -1,5 +1,15 @@
 const md5 = require('md5');
 
-const generateHash = (data) => md5(data);
+const generateHash = (data) => {
+  const { password } = data;
+
+  const hashedData = data;
+
+  const userPassword = md5(password);
+
+  hashedData.password = userPassword;
+  
+  return hashedData;
+};
 
 module.exports = generateHash;
