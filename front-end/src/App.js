caed2cbfea068fd15c3064709a8062dbde9c808c
Sergio Francisco <spinheiro.lf@gmail.com>
2022-09-30 15:42:11 -0300

    apgando app duplicado

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,3 @@
-import React from 'react';
-
-import Router from './Router';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <Router />
-    </div>
-
 import { Switch, Route, Redirect } from 'react-router-dom';
 import AppProvider from './context/AppProvider';
 import CustomerTable from './Pages/CustomerCheckout/customerCheckoutPage';
