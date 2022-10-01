9c9c417fb27c961ed6761738c4a0f513f26186fd
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-10-01 00:16:26 -0300

    adicioada rota de acesso a pagina de pedidos do cliente

--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Customer from './pages/Customer';
+import CustomerOrders from './pages/CustomerOrders';
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Route path="/login" element={ <Login /> } />
       <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/customer/orders" element={ <CustomerOrders /> } />
       <Route path="/customer/*" element={ <Customer /> } />
     </Routes>
   );
