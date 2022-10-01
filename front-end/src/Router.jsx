25cf1cabbb2a8f97cb0d888ee6c065008dbe4d98
Sergio Francisco <spinheiro.lf@gmail.com>
2022-09-30 23:26:45 -0300

    rota de customer

--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Customer from './pages/Customer';
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
       <Route path="/login" element={ <Login /> } />
       <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/customer/*" element={ <Customer /> } />
     </Routes>
   );
 }
