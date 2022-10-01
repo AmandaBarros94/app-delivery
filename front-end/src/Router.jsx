18279c4fb790c8f573f45e69d75fc343130ff920
Sergio Francisco <spinheiro.lf@gmail.com>
2022-10-01 17:51:26 -0300

    mudando rota

--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Customer from './pages/Customer';
+import Seller from './pages/Seller';
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
       <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/*" element={ <Customer /> } />
+      <Route path="/seller/*" element={ <Seller /> } />
     </Routes>
   );
 }
