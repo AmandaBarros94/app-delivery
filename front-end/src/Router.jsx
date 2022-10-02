f794f0fbca9ca697a6b2442874cad3e96637b4e2
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-10-01 21:23:31 -0300

    criada rota de administrador

--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -3,15 +3,17 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Customer from './pages/Customer';
 import Seller from './pages/Seller';
+import Admin from './pages/Admin';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
-      <Route path="/" element={ <Navigate to="/login" /> } />
+      <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/*" element={ <Customer /> } />
       <Route path="/seller/*" element={ <Seller /> } />
+      <Route path="/admin/manage" element={ <Admin /> } />
     </Routes>
   );
 }
